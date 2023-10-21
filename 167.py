c7b5ff18da1c1e5a2426f81a01de03fabad4498c
JimNtantakas <114806458+JimNtantakas@users.noreply.github.com>
2023-10-21 21:14:22 +0300

    Add files via upload

---
+++ 167.py
@@ -0,0 +1,13 @@
+def twoSum(numbers, target):
+    l=0
+    r=len(numbers)-1
+    while l<r:
+        if numbers[l]+numbers[r]<target:
+            l+=1
+        elif numbers[l]+numbers[r]>target:
+            r-=1
+        else:
+            return [l+1,r+1]
+    
+result=twoSum([2,7,11,15],9)
+print(result)
