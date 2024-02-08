2b7bef3cd15718ccc41b7b1580036c2fb154f24c
JimNtantakas <114806458+JimNtantakas@users.noreply.github.com>
2024-02-08 19:06:37 +0000

    Add files via upload

---
+++ 122.py
@@ -0,0 +1,12 @@
+def Best_Time_to_Buy_and_Sell_Stock_II(prices):
+    profit=0
+    for i in range(1,len(prices)):
+        if prices[i]>prices[i-1]:
+            profit+=prices[i]-prices[i-1]
+    return profit
+
+prices=[5,7,100]
+result=Best_Time_to_Buy_and_Sell_Stock_II(prices)
+print(result)
+    
+    
